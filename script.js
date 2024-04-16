20ebf815ccaf183d543107fb5b295b725c57b8d4
ShawnArunjith <shawnarun09@gmail.com>
2024-04-16 19:54:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+/***Objects -> A Collection of related properties or methods
+ can represent real world objects (Animal, Places, People)***/
+
+//this
+//Constructors
+//classes
+//static keyword
+//inheritance
+//Super
+//Getters & Setters
+//Destructring
+//Nested Objects
+//Arrays of Objects
+//Sorting
+
+const person={
+    firstName : "Gajanan",
+    lastName:"Silva",
+    age:30,
+    isEmployed:true,
+    sayHello: function(){console.log("Hi I am Gajanan")}
+}
+
+const person2={
+    firstName : "Shawn",
+    lastName:"Arunjith",
+    age:28,
+    isEmployed:false,
+}
+
+person.sayHello();
+
+console.log(person.firstName)
